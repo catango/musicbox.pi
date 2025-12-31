9763cf2739c59e74f5816defad666155d90e3f33
chpr <chriscatango@gmail.com>
2025-12-31 16:38:16 +0100

    clean up image creation

--- stage2-musicbox/00-musicbox/01-run.sh
+++ stage2-musicbox/00-musicbox/01-run.sh
@@ -2,33 +2,15 @@
 
 HOME="${ROOTFS_DIR}/home/${FIRST_USER_NAME}"
 
-#echo "${MUSIC_SERVER_USER_PASS}" > "${HOME}/user_pass"
-#echo "${MUSIC_SERVER_JUSER_PASS}" > "${HOME}/juser_pass"
-#envsubst < files/config-musicserver.sh > "${ROOTFS_DIR}/usr/local/bin/config-musicserver.sh"
-#install -m 644 files/config-musicserver.service "${ROOTFS_DIR}/etc/systemd/system/"
-
-#install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
-#install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
-
-#HOME="${ROOTFS_DIR}/home/${FIRST_USER_NAME}"
-#install -m 755 -o 1000 -g 1000 files/kiosk.sh "${HOME}/"
-
 # install config
 install -m 644 files/musicbox.txt "${ROOTFS_DIR}/boot/firmware/"
 
 # install script for ssh key generation & setup
-#envsubst < files/firstrun.sh > "${TMP_FILE}"
-#install -m 755 -o 1000 -g 1000 "${TMP_FILE}" "${HOME}/firstrun.sh"
 install -m 755 -o 0 -g 0 files/firstboot.sh "${ROOTFS_DIR}/usr/local/bin/"
 install -m 644 -o 0 -g 0 files/firstboot.service "${ROOTFS_DIR}/etc/systemd/system/"
 install -m 755 -o 0 -g 0 files/firstlogin.sh "${ROOTFS_DIR}/usr/local/bin/"
 install -m 755 -o 0 -g 0 files/musicbox-reconfigure.sh "${ROOTFS_DIR}/usr/local/bin/"
 install -m 644 -o 1000 -g 1000 files/.bashrc "${HOME}/"
-
-# configure ssh
-#install -m 755 -o 1000 -g 1000 -d "${HOME}/.ssh/"
-#envsubst < files/ssh_config > "${TMP_FILE}"
-#install -m 644 -o 1000 -g 1000 "${TMP_FILE}" "${HOME}/.ssh/config"
 
 # configure mpd tunnel
 install -m 644 -o 0 -g 0 files/mpd-tunnel.service "${ROOTFS_DIR}/etc/systemd/system/"
--- stage2-musicbox/00-musicbox/02-run-chroot.sh
+++ stage2-musicbox/00-musicbox/02-run-chroot.sh
@@ -11,23 +11,17 @@ systemctl enable pulseaudio.service
 
 # mpd
 usermod -aG pulse-access mpd
-#systemctl enable mpd-tunnel
-#systemctl enable mpd
 
 # librespot
-#systemctl enable librespot
+# no permissions to add
 
 #scream
 usermod -aG pulse-access scream
-#systemctl enable scream
 
 # bluetooth
 usermod -aG bluetooth pulse
-#systemctl enable bluetooth.service
-#systemctl enable bt-agent@hci0.service
 
 # shairport-sync
 usermod -aG pulse-access shairport-sync
-#systemctl enable shairport-sync
 
 systemctl enable firstboot.service
