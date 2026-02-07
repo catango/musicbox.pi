4dc19ba2ca9d177f2d4783389bc4211f3c5e171b
chpr <chriscatango@gmail.com>
2026-02-07 13:17:55 +0100

    Update raspberry pi os to debian trixie
    Debian custom packages for scream and librespot are updated to trixie
    builds.
    Change Pi-gen config, init script and github action to build trixie by default.

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ## clone pi-gen raspberry pi build system
-git clone --depth 1 --branch 2024-07-04-raspios-bookworm https://github.com/RPI-Distro/pi-gen.git
+git clone --depth 1 --branch 2025-12-04-raspios-trixie-armhf https://github.com/RPI-Distro/pi-gen.git
 
 ## disable export for stage2
 touch ./pi-gen/stage2/SKIP_IMAGES ./pi-gen/stage2/SKIP_NOOBS
